a8b09cfd39e862ce7e529c9f064635ad2fd8dbc1
vini.dotto <vinidotto02@gmail.com>
2024-11-20 16:49:40 -0300

    adicao de paginacao

--- backend/avaliacoes/models.py
+++ backend/avaliacoes/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Avaliacao(models.Model):
-    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
-    comentario = models.TextField(null = True, blank = True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    comentario = models.TextField(null=True, blank=True)
     nota = models.DecimalField(max_digits=3, decimal_places=2)
-    data =  models.DateTimeField(auto_now_add=True)
+    data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.user.username
+        return f"Avaliacao de {self.usuario.username} - Nota: {self.nota}"
